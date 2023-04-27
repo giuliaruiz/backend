b0e6183459ee05e12e0528a32bd7a9441dee45f0
giuliaruiz <131925974+giuliaruiz@users.noreply.github.com>
2023-04-26 21:52:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.listen(PORT, () => {
     console.log(`Server Started at ${PORT}`)
 })
 
-var mongoURL = "mongodb+srv://giulia216080:giulia01@cluster0.aqhcpp7.mongodb.net/tarefas"
+var mongoURL = "mongodb+srv://giulia216080:giulia01@cluster0.aqhcpp7.mongodb.net/tarefasDB/tarefas"
 //Configurando a conexao com o Banco de Dados
 var mongoose = require('mongoose');
 mongoose.connect(mongoURL, {
